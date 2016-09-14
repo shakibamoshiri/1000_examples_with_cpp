c236f50bc02a6255e45d3bef0c2d4a1a9a2f84b2
Shekib <vhsu30@yahoo.com>
2016-09-15 00:00:33 +0430

    number: 881

---
+++ Language/08_exception/temp.cpp
@@ -0,0 +1,8 @@
+/*
+Exception:
+    01 try-block
+    02 function-try-block
+    03 noexcept specifier ( since c++11 )
+    04 noexcept operator  ( since c++11 )
+    05 dynamic exception specifier
+*/
