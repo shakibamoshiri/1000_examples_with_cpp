2e874cb145deb300f04c13ac9a8db9c21b85936a
Shekib <vhsu30@yahoo.com>
2016-10-19 20:25:28 +0330

    rename some file, fix list of examples

--- up.sh
+++ up.sh
@@ -1,6 +1,7 @@
 du -sh > size_of_the_whole_examples.txt
 tree -d > list_tree.txt
 tree -sh > list_full_tree.txt
-size=$(ls -R | ack -c '.cpp$')
-mv -f *_c++_examples_are_here_now $size\_c++_examples_are_here_now
-perl -le 'print scalar localtime' > $size\_c++_examples_are_here_now
+
+touch $(find . -name *.cpp | perl -lne '!/temp.cpp/ && ++$c;END{printf "%04d_examples_are_here_now",$c}')
+perl -le 'print scalar localtime' > ????_examples_are_here_now
+find . -name *.cpp | perl -lne '!/temp.cpp/ && printf "%04d%s%s%s\n", ++$c," ",$_' > full_list_in_order.txt
