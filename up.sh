88b13d8042781e27b054f173b4e1133b444f7b27
Shekib <vhsu30@yahoo.com>
2017-01-28 13:20:58 +0330

    remove some useless command

--- up.sh
+++ up.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-tree -d > list_tree.txt
-tree -sh > list_full_tree.txt
+utctime
 
-rm -f *_examples_are_here_now*
-touch $(find . -name *.cpp | perl -lne '!/temp.cpp/ && ++$c;END{printf "%04d_examples_are_here_now",$c}')
-perl -le 'print scalar localtime' > *_examples_are_here_now*
+example=$(find . -name *.cpp | perl -lne '!/temp.cpp/ && ++$c;END{printf "%04d_examples_are_here_now",$c}')
+echo $example
 
 size=$(du -sh)
-rm -f repository_size_is*
-perl -le 'print scalar localtime' > repository_size_is_${size:0:4}
+echo repository_size_is_${size:0:4}
 
-find . -name *.cpp | perl -lne '!/temp.cpp/ && printf "%04d%s%s%s\n", ++$c," ",$_' > full_list_in_order.txt
+echo ........................................
